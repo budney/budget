542359a8c8e0bc6bd56d0118211c44b3d92d1c8b
Len Budney <Len.Budney@grantstreet.com>
2018-01-30 21:44:12 -0500

    Moved transaction info into transaction.go.

--- budget/spreadsheet.go
+++ budget/spreadsheet.go
@@ -30,17 +30,6 @@ type Spreadsheet struct {
 	sheets.Service // Adds the Google Sheets API to this struct
 }
 
-// A Transaction contains information about a single transaction.
-type Transaction struct {
-	Index          int       // A counter for sorting transactions on the same Date
-	Date           time.Time // The date of the transaction
-	Type           string    // A type description, such as POS, Check, ATM, etc.
-	Description    string    // Usually the payor / payee of the transaction
-	DebitPennies   int64     // The debit amount, in pennies
-	CreditPennies  int64     // The credit amount, in pennies
-	BalancePennies int64     // The balance, in pennies, after the transaction
-}
-
 // AppendFromChannel runs a goroutine that listens to a channel for
 // transactions, filters out the ones that don't apply, and appends
 // the rest to the budget spreadsheet for the specified account. It
---
+++ budget/transaction.go
@@ -0,0 +1,16 @@
+// Copyright 2017 Len Budney. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package budget
+
+// A Transaction contains information about a single transaction.
+type Transaction struct {
+	Index          int       // A counter for sorting transactions on the same Date
+	Date           time.Time // The date of the transaction
+	Type           string    // A type description, such as POS, Check, ATM, etc.
+	Description    string    // Usually the payor / payee of the transaction
+	DebitPennies   int64     // The debit amount, in pennies
+	CreditPennies  int64     // The credit amount, in pennies
+	BalancePennies int64     // The balance, in pennies, after the transaction
+}
